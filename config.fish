4c73fdeee639729cd9dfe0ac4efbd080d8334601
geekplux <geekplux@qq.com>
2016-02-01 18:37:03 +0800

    update

--- config.fish
+++ config.fish
@@ -1,2 +1,11 @@
 set -g -x PATH /usr/local/bin $PATH
 set -g -x fish_greeting 'Talk is cheap, show me the Code!'
+
+# Path to Oh My Fish install.
+set -gx OMF_PATH "/Users/geekplux/.local/share/omf"
+
+# Customize Oh My Fish configuration path.
+set -gx OMF_CONFIG "/Users/geekplux/Dropbox/project/fun/dotfiles/omf"
+
+# Load oh-my-fish configuration.
+source $OMF_PATH/init.fish
