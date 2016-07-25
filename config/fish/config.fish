86ce9584867272eecb0d626b62d4e9bc47150f72
geekplux <geekplux@qq.com>
2016-07-25 12:17:36 +0800

    fix fish omf config path

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,7 +7,7 @@ set -g -x fish_greeting 'Talk is cheap, show me the Code!'
 set -gx OMF_PATH "/Users/geekplux/.local/share/omf"
 
 # Customize Oh My Fish configuration path.
-set -gx OMF_CONFIG "/Users/geekplux/Dropbox/project/fun/dotfiles/omf"
+set -gx OMF_CONFIG "/Users/geekplux/.omf"
 
 
 # alias
